468e726e85df7cd3a01cdf102c61774582f36756
Cindy1o1 <709334661@qq.com>
2023-06-26 19:59:36 +0800

    注释

---
+++ src/main/java/com/example/springbootdemo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.springbootdemo.controller;
+
+import com.example.springbootdemo.entity.User;
+import com.example.springbootdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userservice;
+
+    @RequestMapping("/findAll")
+    public List<User> findAll(){
+        List<User> list = new ArrayList<>();
+        list=userservice.findAll();
+        System.out.println("dddd");
+        return list;
+    }
+}
+
